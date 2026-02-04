53be699e68466ffbaa6f8f39e12c3cda5457ec76
Thomas O'Grady <tomogrady19@gmail.com>
2026-02-04 16:59:53 +0000

    deleted JS/node backend as I'm now doing the backend entirely in C

--- backend/src/routes/stocks.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getStockHistory } from "../controllers/stocksController.js";
-
-const router = Router();
-
-router.get("/history", getStockHistory);
-
-export default router;
